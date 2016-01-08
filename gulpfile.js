62e4ecb28fef39d1a6c0e07655d85dade227ae1b
Chris Humboldt <chrismodem@gmail.com>
2016-01-08 14:00:40 +0200

    Updates to Bower configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@
  */
 
 // Table of contents
-// ---------------------------------------------------------------------------------------
 // Requires
 
 // Requires
@@ -18,28 +17,28 @@ var uglify = require('gulp-uglify');
 gulp.task('build', ['css', 'js']);
 
 gulp.task('css', function() {
-   gulp.src('./sass/*.scss')
-      .pipe(sass({
-         outputStyle: 'compressed'
-      }))
-      .pipe(gulp.dest('./css'))
-      .pipe(livereload());
+	gulp.src('./sass/*.scss')
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(gulp.dest('./css'))
+		.pipe(livereload());
 });
 
 gulp.task('js', function() {
-   gulp.src('./js/*.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('./js/min'))
-      .pipe(livereload());
+	gulp.src('./js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./js/min'))
+		.pipe(livereload());
 });
 
 gulp.task('watch', function() {
-   livereload.listen();
-   gulp.watch([
+	livereload.listen();
+	gulp.watch([
       './sass/*.scss',
       './sass/**/*.scss'
    ], ['css']);
-   gulp.watch([
+	gulp.watch([
       './js/*.js'
    ], ['js']);
 });
